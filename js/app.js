d2282631d78abe68919af8a9b5927210a8c7d2d2
flutterkat <85975055+flutterkat@users.noreply.github.com>
2021-08-30 11:55:00 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ const pages = document.getElementsByClassName('badge');
 const p = document.getElementsByClassName('description');
 const cube = document.getElementById('photo-cube');
 const cubeContainer = document.getElementsByClassName('cube-container');
-const cubeControl = document.getElementsByClassName('bp-list')[0];
+const cubeFocus = document.getElementsByTagName('a');
 let RNG = function () {
     return Math.floor(Math.random() * (180 - (-180)) + (-180));
-  }
+  };
   
 
  function rotator() {
-    setInterval (function () {if (p[0].style.display === 'none' && p[1].style.display === 'none' && p[2].style.display === 'none' && p[3].style.display === 'none' && p[4].style.display === 'none' && p[5].style.display === 'none') {
+    setInterval (function () {if (p[0].style.display === 'none' && p[1].style.display === 'none' && p[2].style.display === 'none' && p[3].style.display === 'none' && p[4].style.display === 'none' && p[5].style.display === 'none' && cubeFocus[0] !== document.activeElement && cubeFocus[1] !== document.activeElement && cubeFocus[2] !== document.activeElement && cubeFocus[3] !== document.activeElement && cubeFocus[4] !== document.activeElement && cubeFocus[5] !== document.activeElement) {
         cube.style.transition = 'transform 20s linear';
         cube.style.transform = `rotateX(${RNG()}deg) rotateY(${RNG()}deg) rotateZ(${RNG()}deg)`;
     } else {
@@ -21,7 +21,7 @@ function startRotator() {
     setTimeout (function () {
         cube.style.transition = 'transform 10s linear';
         cube.style.transform = `rotateX(${RNG()}deg) rotateY(${RNG()}deg)`;
-    }, 100)
+    }, 100);
 }
 
 startRotator();
@@ -45,7 +45,14 @@ pages[0].addEventListener('click', ()=>{
         p[0].style.display = 'none';
         startRotator();
     }
-})
+});
+
+cubeFocus[0].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateX(0deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+    });
 
 pages[1].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
@@ -63,7 +70,14 @@ pages[1].addEventListener('click', ()=>{
         p[1].style.display = 'none';
         startRotator();
     }
-})
+});
+
+cubeFocus[1].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateX(90deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+    });
 
 pages[2].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
@@ -77,12 +91,19 @@ pages[2].addEventListener('click', ()=>{
             }
         }
         p[2].style.display = '';
-        return
+        return;
     } else {
         p[2].style.display = 'none';
         startRotator();
     }
-})
+});
+
+cubeFocus[2].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateY(90deg)';
+    cubeContainer[0].style.marginLeft = '50px';
+    cubeContainer[0].style.marginRight = '';
+    });
 
 pages[3].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
@@ -100,7 +121,14 @@ pages[3].addEventListener('click', ()=>{
         p[3].style.display = 'none';
         startRotator();
     }
-})
+});
+
+cubeFocus[3].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateY(-90deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '50px';
+});
 
 pages[4].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
@@ -118,7 +146,14 @@ pages[4].addEventListener('click', ()=>{
         p[4].style.display = 'none';
         startRotator();
     }
-})
+});
+
+cubeFocus[4].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateY(180deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+});
 
 pages[5].addEventListener('click', ()=>{
     cube.style.transition = 'transform 1s ease-in-out';
@@ -136,4 +171,11 @@ pages[5].addEventListener('click', ()=>{
         p[5].style.display = 'none';
         startRotator();
     }
-})
+});
+
+cubeFocus[5].addEventListener('focus', ()=>{
+    cube.style.transition = 'transform 1s ease-in-out';
+    cube.style.transform = 'rotateX(-90deg)';
+    cubeContainer[0].style.marginLeft = '';
+    cubeContainer[0].style.marginRight = '';
+});
